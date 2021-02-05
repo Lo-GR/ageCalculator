deb7f29f595db1a0b8da39606aa6e124acb6debd
Lo-GR <diamondintheroth@gmail.com>
2021-02-05 10:36:46 -0800

    write code for venusAge and pass test

--- src/js/agecalc.js
+++ src/js/agecalc.js
@@ -3,11 +3,12 @@ export class AgeCalculator {
     this.age = age;
   }
   mercuryAge(){
-    let ageReturn = this.age/.24
+    let ageReturn = this.age/.24;
     return Math.round(ageReturn);
   }
   venusAge(){
-    
+    let ageReturn = this.age/.62;
+    return Math.round(ageReturn);
   }
   
 }
