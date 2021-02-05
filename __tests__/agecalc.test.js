e8985a8d31ba78e83caf95eb043dddfbc9d701c2
Lo-GR <diamondintheroth@gmail.com>
2021-02-05 10:11:34 -0800

    make test for mercury age calculator

--- __tests__/agecalc.test.js
+++ __tests__/agecalc.test.js
@@ -8,7 +8,9 @@ describe('AgeCalculator', () => {
 
   test('should construct an object with age properties entered', () => {
     expect(testObject.age).toEqual(20);
-
   });
 
+  test('should return age in Mercury years based on age of object, rounded to largest integer', () => {
+    expect(testObject.age).toEqual(83)
+  }
 });
