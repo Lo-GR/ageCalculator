5278151907edabd5d5d6e018d7b95c3fdb1155b8
Lo-GR <diamondintheroth@gmail.com>
2021-02-05 09:00:29 -0800

    preplan on whiteboard and begin writing test

---
+++ __tests__/agecalc.test.js
@@ -0,0 +1,14 @@
+import {AgeCalculator} from '../src/js/agecalc.js';
+
+describe('AgeCalculator', () => {
+ 
+  test('should construct an object with age properties entered', () => {
+    expect(NAME).toEqual(TEST);
+
+  });
+
+  beforeEach(() => {
+    Var = etc;
+  });
+
+});
