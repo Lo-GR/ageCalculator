e84e5828ec3433d5df40f2b5e1f7d1b3b3deb2c9
Lo-GR <diamondintheroth@gmail.com>
2021-02-05 16:27:18 -0800

    add some user logic to main js for taking in age and le

--- src/main.js
+++ src/main.js
@@ -4,7 +4,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import { AgeCalculator } from './js/agecalc';
 
 $(document).ready(function(){
-
+  $("#ageForm").submit(function(event){
+    event.preventDefault();
+    const ageObject = new AgeCalculator($("#age").val(), ("#le").val())
+  })
 });
