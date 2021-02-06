a3b7e767131a058e3d5cea4238ed8062aad5365c
Lo-GR <diamondintheroth@gmail.com>
2021-02-05 18:53:15 -0800

    fix minor indentation

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-
 import './css/styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { AgeCalculator } from './js/agecalc';
+
 $(document).ready(function(){
   $("#ageForm").submit(function(event){
     event.preventDefault();
